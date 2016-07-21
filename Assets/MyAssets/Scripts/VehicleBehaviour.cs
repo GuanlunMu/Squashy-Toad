4cd43929f7426e67d6e34e110054ad853913ae20
GuanlunMu <guanlunmu@gmail.com>
2016-07-21 10:47:12 -0500

    adding new fire wall features

---
+++ Assets/MyAssets/Scripts/VehicleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleBehaviour : MonoBehaviour {
+
+	float speed;
+	float length;
+
+	// Use this for initialization
+	void Start () {
+		float lifeTime = length / speed;
+		Invoke ("remove", lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += (Vector3.right * speed * Time.deltaTime);
+	}
+
+	void remove() {
+		Destroy (gameObject);
+	}
+	public void setSpeed(float speed, float length) {
+		this.speed = speed;
+		this.length = length;
+	}
+}
--- Assets/MyAssets/Scripts/VehicleSpawner.cs
+++ Assets/MyAssets/Scripts/VehicleSpawner.cs
@@ -3,9 +3,23 @@ using System.Collections;
 
 public class VehicleSpawner : MonoBehaviour {
 
+	public GameObject[] vehiclePrefabs;
+
+	float heightOffset = 1; 
+	float startOffset = -10;
+	public float vehicleSpeed = 5.0f;
+	public float vehicleLifeLength = 10.0f;
+
+	void Awake() {
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		Vector3 vehiclePositon = transform.position + Vector3.up * heightOffset + Vector3.right * startOffset;
+		GameObject vehicleObject = Instantiate (vehiclePrefabs[0], vehiclePositon , transform.rotation) as GameObject;
+		VehicleBehaviour vehicleScript = vehicleObject.GetComponent<VehicleBehaviour> ();
+		vehicleScript.setSpeed (vehicleSpeed, vehicleLifeLength);
+			
 	}
 	
 	// Update is called once per frame
